86d534f88ad9730b9b307a5e613a0dcae0884a66
Aryanbose <77531896+Aryanbose@users.noreply.github.com>
2021-10-17 18:30:40 +0530

    Add files via upload

---
+++ spotify clone/script.js
@@ -0,0 +1,17 @@
+console.log("Hey Welcome To Spotify Clone")
+let songs =[
+    {songName:"Often", filePath:"needed particles/often.mp3", coverPath:"needed particles/1.jpg"},
+    {songName:"Often", filePath:"needed particles/often.mp3", coverPath:"needed particles/1.jpg"},
+    {songName:"Often", filePath:"needed particles/often.mp3", coverPath:"needed particles/1.jpg"},
+    
+    {songName:"Often", filePath:"needed particles/often.mp3", coverPath:"needed particles/1.jpg"},
+    {songName:"Often", filePath:"needed particles/often.mp3", coverPath:"needed particles/1.jpg"},
+    {songName:"Often", filePath:"needed particles/often.mp3", coverPath:"needed particles/1.jpg"},
+    {songName:"Often", filePath:"needed particles/often.mp3", coverPath:"needed particles/1.jpg"},
+    {songName:"Often", filePath:"needed particles/often.mp3", coverPath:"needed particles/1.jpg"},
+    {songName:"Often", filePath:"needed particles/often.mp3", coverPath:"needed particles/1.jpg"},
+    {songName:"Often", filePath:"needed particles/often.mp3", coverPath:"needed particles/1.jpg"},
+]
+let audioElement = new Audio ('./needed particles/often.mp3')
+
+audioElement.play();
